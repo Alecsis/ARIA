a1727806e8da07ae43bd5edc67794e8537d12b6f
alecsis <josephddang00@gmail.com>
2026-05-16 23:51:13 -0400

    gyro

---
+++ main.py
@@ -0,0 +1,26 @@
+from machine import Pin, I2C
+from time import sleep_ms
+from MPU6050 import MPU6050  # your file name
+
+
+i2c = I2C(
+    0,
+    scl=Pin(5), 
+    sda=Pin(4),
+    freq=400000
+)
+print("I2C Scan:", i2c.scan())
+mpu = MPU6050(i2c)
+print("MPU6050 initialized")
+while True:
+    accel = mpu.read_accel_data()   # m/s^2
+    gyro = mpu.read_gyro_data()     # deg/s
+    temp = mpu.read_temperature()   # °C
+    print("----- MPU6050 -----")
+    print("Accel (m/s^2):", accel)
+    print("Gyro  (deg/s):", gyro)
+    print("Temp  (C):", temp)
+    angle = mpu.read_angle()
+    print("Angle (rad):", angle)
+    print("-------------------\n")
+    sleep_ms(100)
