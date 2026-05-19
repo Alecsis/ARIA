0b26d1d468d2f42e55b78222263eed190e54254a
alecsis <josephddang00@gmail.com>
2026-05-18 22:37:14 -0400

    dead

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-from machine import Pin, I2C
-from time import sleep_ms
-from MPU6050 import MPU6050  # your file name
+from radio import Radio
+import time
 
+radio = Radio()
+counter = 0
 
-i2c = I2C(
-    0,
-    scl=Pin(5), 
-    sda=Pin(4),
-    freq=400000
-)
-print("I2C Scan:", i2c.scan())
-mpu = MPU6050(i2c)
-print("MPU6050 initialized")
+print("Starting radio test...")
+
 while True:
-    accel = mpu.read_accel_data()   # m/s^2
-    gyro = mpu.read_gyro_data()     # deg/s
-    temp = mpu.read_temperature()   # °C
-    print("----- MPU6050 -----")
-    print("Accel (m/s^2):", accel)
-    print("Gyro  (deg/s):", gyro)
-    print("Temp  (C):", temp)
-    angle = mpu.read_angle()
-    print("Angle (rad):", angle)
-    print("-------------------\n")
-    sleep_ms(100)
+    msg = "AVO {}".format(counter)
+    print("Sending:", msg)
+
+    radio.send(msg.encode())
+
+    counter += 1
+    time.sleep(0.5)
