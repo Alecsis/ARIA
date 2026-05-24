8feb88a07233e49c780021a72a61997156e27906
alecsis <josephddang00@gmail.com>
2026-05-24 15:30:24 -0400

    updfate

--- main.py
+++ main.py
@@ -8,19 +8,35 @@ print("RX ready")
 while True:
     if radio.available():
         data = radio.recv()
-        if data:
-            try:
-                msg = data.decode()
-                parts = msg.split("|")
 
-                if parts[0] == "AVO":
-                    counter = parts[1]
-                    print("Packet OK → Counter:", counter)
+        if not data:
+            continue
 
-                else:
-                    print("Unknown packet:", msg)
+        try:
+            msg = data.decode().strip()
 
-            except Exception as e:
-                print("Decode error:", e)
+            parts = msg.split(",")
+
+            if parts[0] == "AVO" and len(parts) >= 8:
+
+                gx = float(parts[1])
+                gy = float(parts[2])
+                gz = float(parts[3])
+                alt = float(parts[4])
+                ax  = float(parts[5])
+                ay  = float(parts[6])
+                az  = float(parts[7])
+
+                print("----- PACKET OK -----")
+                print("Gyro:", gx, gy, gz)
+                print("Accel:", ax, ay, az)
+                print("Alt(ft):", alt)
+                print("---------------------")
+
+            else:
+                print("Bad packet format:", msg)
+
+        except Exception as e:
+            print("Decode error:", e)
 
     time.sleep(0.05)
